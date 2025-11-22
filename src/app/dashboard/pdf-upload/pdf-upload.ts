92a3c04654bf4a75513384448708788156075458
Olar-Alex-Student <alex.olar@student.usv.ro>
2025-11-22 18:00:18 +0200

    first commit

---
+++ src/app/dashboard/pdf-upload/pdf-upload.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-upload',
+  imports: [],
+  templateUrl: './pdf-upload.html',
+  styleUrl: './pdf-upload.css',
+})
+export class PdfUpload {
+
+}
---
+++ src/app/dashboard/subscriptions/subscriptions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriptions',
+  imports: [],
+  templateUrl: './subscriptions.html',
+  styleUrl: './subscriptions.css',
+})
+export class Subscriptions {
+
+}
